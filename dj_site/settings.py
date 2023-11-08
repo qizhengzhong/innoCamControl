4508e043ffb8e4a41574bf44247fb8cad92485c3
Chris Qi <zhengzhongqi@microsoft.com>
2023-11-08 09:03:24 +0000

    Chinese support for the panel

--- dj_site/settings.py
+++ dj_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,8 +118,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
+LANGUAGES = [
+    ('en', _('English')),
+    ('zh', _('Chinese')),
+]
 
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
